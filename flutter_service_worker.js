fbd78289e74d6ea20cb52c0090cdd57a3bae8e71
michaelchrisdev <michaelchris1417@gmail.com>
2026-09-12 23:26:05 +0700

    fix kontraktor insert bug, add approved at

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -28,14 +28,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "890cff81859b6240fef6c76961d4541e
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "072516ad635e9adf88d3acfaf2ba6319",
+"flutter_bootstrap.js": "3018ee9f4726bffacd7ee03abfb98c13",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "7f746711fa4011f514bcf360558ff41a",
 "/": "7f746711fa4011f514bcf360558ff41a",
-"main.dart.js": "185273fcbb356543a8d10b641bcc994e",
+"main.dart.js": "db3219ff98c39904b5aea8cd33c5047e",
 "manifest.json": "caf9cd24beeb586a3a380a59380d3745",
 "version.json": "e892e3aaa807c0c1fcf51b1be348d320"};
 // The application shell files that are downloaded before a service worker can
